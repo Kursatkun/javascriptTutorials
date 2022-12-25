3b9c6c6d375c22816d9b8a331d74dcf278c2d531
Kursatkun <a.kursat.u@gmail.com>
2022-12-25 05:08:57 +0300

    ugly and long

--- jsCalculator/app.js
+++ jsCalculator/app.js
@@ -3,34 +3,73 @@ const screenDisplay = document.querySelector('.screen')
 
 let calculation = []
 let calcualtionString = ""
-let accumulativeCalculation = []
+let accumulativeCalculation
+let calculationQueue = []
 
+let operatorButtons = ["+", "-", "*", "/"];
 
-let operatorButtons = ["+", "-", "*", ".", "/"];
-
-function checkOperator (value) {
-    if (operatorButtons.includes(value)) {
-        const pieces = calculation.toString.split(/[+-*./]/)
-        const last = pieces[pieces.length - 1]
+function decideOperation (value, num1, num2) {
+    switch (true) {
+        case value === "+":
+            return (parseInt(num1, 10) + parseInt(num2, 10)).toString()
+        case value === "-":
+            return (parseInt(num1, 10) - parseInt(num2, 10)).toString()
+        case value === "*":
+            return (parseInt(num1, 10) * parseInt(num2, 10)).toString()
+        case value === "/":
+            return (parseInt(num1, 10) / parseInt(num2, 10)).toString()       
     }
+}
+
+function emptyArray (array) {
+    while (array.length) {
+    array.pop();
+    }
+    console.log("array is reseted")
+    console.log(array)
 }
 
 function isNumeric(num){
     return !isNaN(num)
-  }
-
+}
+123+123
 function addToScreen (value){
-    calculation.push(value)
-    accumulativeCalculation = calculation.join('')
-    screenDisplay.textContent = accumulativeCalculation
+    let temp
+    if ((calculationQueue.length === 2) && operatorButtons.includes(value)) {
+        calculationQueue.push(calculation.join(''))
+        temp = decideOperation(calculationQueue[1], calculationQueue[0], calculationQueue[2])
+        emptyArray(calculationQueue)
+        emptyArray(calculation)
+        calculationQueue.push(temp)
+        calculationQueue.push(value)
+        accumulativeCalculation = calculationQueue.join('')
+        screenDisplay.textContent = accumulativeCalculation
+        console.log("here1")
+        console.log(calculation)
+        console.log(calculationQueue)
+    } else if ((operatorButtons.includes(value)) && calculationQueue.length <2) {
+        calculationQueue.push(calculation.join(''))
+        calculationQueue.push(value)
+        emptyArray(calculation)
+        accumulativeCalculation = calculationQueue.join('')
+        screenDisplay.textContent = accumulativeCalculation
+        console.log("here2")
+        console.log(calculationQueue)
+    } else {
+        calculation.push(value)
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation
+        console.log("here3")
+        console.log(calculation)
+    }
+
 }
 
 function action(button) {
     const value = button.textContent
-
     if (value === "CLEAR") {
         calculation = []
-        screenDisplay.textContent = '.'
+        screenDisplay.textContent = '0'
     } else if (value === "=") {
         screenDisplay.textContent = calculate()
     } else {
